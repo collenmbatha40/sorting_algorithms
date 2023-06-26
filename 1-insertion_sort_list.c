92f075dfa6acbd9ec8c0cdd0ea45643e7cf9b117
Anele Mbatha <collenmbatha40@gmail.com>
2023-06-26 01:10:52 -0700

    Updated task 1 files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * swap_nodes - swaps 2 nodes in a listint_t doubly-linked list.
- * @h: pointer to the head of the doubly-linked list.
- * @n1: pointer to first node to swap.
- * @n2: second node to swap.
+ * swap_nodes - swaps 2 nodes in a listint_t doubly-linked list
+ * @h: pointer to the head of the doubly-linked list
+ * @n1: pointer to first node to swap
+ * @n2: second node to swap
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
